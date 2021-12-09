9640f199984f858565ff75269d101746b91eca3f
Alvadda <58438582+Alvadda@users.noreply.github.com>
2021-12-09 22:03:37 +0100

    add firebase google auth

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,46 @@
+import { initializeApp } from 'firebase/app'
+import { firebaseConfig } from './firebase.config'
+import { getAuth, signInWithPopup, GoogleAuthProvider, User } from 'firebase/auth'
+
+const app = initializeApp(firebaseConfig)
+console.log(firebaseConfig)
+
+const auth = getAuth()
+const provider = new GoogleAuthProvider()
+
 import './style/style.scss'
+import { useEffect, useState } from 'react'
 
 export const App = () => {
-  return <h1>React TypeScript Template</h1>
+  const [user, setUser] = useState<User | null>()
+  const [error, setError] = useState()
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((user) => {
+      setUser(user)
+    })
+    return unsubscribe
+  }, [])
+
+  const signInWithGoogle = async () => {
+    try {
+      await signInWithPopup(auth, provider)
+    } catch (error: any) {
+      setError(error.message)
+    }
+  }
+  return (
+    <>
+      <h1>React TypeScript Template</h1>
+      {user && (
+        <div>
+          <p>User: {user.displayName}</p>
+          <p>Email: {user.email}</p>
+          <button onClick={() => auth.signOut()}>Sign Out</button>
+        </div>
+      )}
+      {!user && <button onClick={() => signInWithGoogle()}>Login</button>}
+      {error && <p>Error: {error}</p>}
+    </>
+  )
 }
